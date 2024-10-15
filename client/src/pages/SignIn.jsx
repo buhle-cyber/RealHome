b149c46f05d075398219ef26e93c2941b3b14501
buhle-cyber <buhlepinana4@gmail.com>
2024-10-15 09:46:58 +0200

    completing prfoile page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -63,7 +63,7 @@ export default function SignUp() {
 
         <button
           disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          className='bg-slate-800 text-white p-3 rounded-lg uppercase hover:bg-slate-700 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
